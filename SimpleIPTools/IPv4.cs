569b94bbdc5e37d02679daff8ec220fcd50e6249
Mahdi Dadkhah <dadkhah@sepanodata.ir>
2024-10-31 00:09:22 +0330

    Add project files.

---
+++ SimpleIPTools/IPv4.cs
@@ -0,0 +1,125 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace SimpleIPTools
+{
+    public static class IPv4
+    {
+        private static long iMask(int s)
+        {
+            return (long)(Math.Pow(2, 32) - Math.Pow(2, (32 - s)));
+        }
+
+        private static string long2ip(long ipAddress)
+        {
+            System.Net.IPAddress ip;
+            if (System.Net.IPAddress.TryParse(ipAddress.ToString(), out ip))
+            {
+                return ip.ToString();
+            }
+            return "";
+        }
+        private static long ip2long(string ipAddress)
+        {
+            System.Net.IPAddress ip;
+            if (System.Net.IPAddress.TryParse(ipAddress, out ip))
+            {
+                return (((long)ip.GetAddressBytes()[0] << 24) | ((long)ip.GetAddressBytes()[1] << 16) | ((long)ip.GetAddressBytes()[2] << 8) | ip.GetAddressBytes()[3]);
+            }
+            return -1;
+        }
+        public static string GetPreviousIP(string ipAddress)
+        {
+            return long2ip(ip2long(ipAddress) - 1);
+        }
+        public static string GetNextIP(string ipAddress)
+        {
+            return long2ip(ip2long(ipAddress) + 1);
+        }
+        public static List<string> Convert2CIDR(string ipStart, string ipEnd)
+        {
+            long start = ip2long(ipStart);
+            long end = ip2long(ipEnd);
+            var result = new List<string>();
+
+            while (end >= start)
+            {
+                byte maxSize = 32;
+                while (maxSize > 0)
+                {
+                    long mask = iMask(maxSize - 1);
+                    long maskBase = start & mask;
+
+                    if (maskBase != start)
+                    {
+                        break;
+                    }
+
+                    maxSize--;
+                }
+                double x = Math.Log(end - start + 1) / Math.Log(2);
+                byte maxDiff = (byte)(32 - Math.Floor(x));
+                if (maxSize < maxDiff)
+                {
+                    maxSize = maxDiff;
+                }
+                string ip = long2ip(start);
+                result.Add(ip + "/" + maxSize);
+                start += (long)Math.Pow(2, (32 - maxSize));
+            }
+            return result;
+        }
+
+        public static string ToUncompressedString(this IPAddress ipAddress)
+        {
+            if (ipAddress.AddressFamily == AddressFamily.InterNetwork)  // IPv4
+            {
+                var strings = ipAddress.GetAddressBytes()
+                    .Select(b => string.Format("{0:D3}", b));   // format bytes with padded 0s
+
+                return string.Join(".", strings);   // join padded strings with '.' character
+            }
+
+            return ipAddress.ToString();    // all else treat as to string
+        }
+
+        public class IPAddressRange
+        {
+            readonly AddressFamily addressFamily;
+            readonly byte[] lowerBytes;
+            readonly byte[] upperBytes;
+
+            public IPAddressRange(IPAddress lowerInclusive, IPAddress upperInclusive)
+            {
+                this.addressFamily = lowerInclusive.AddressFamily;
+                this.lowerBytes = lowerInclusive.GetAddressBytes();
+                this.upperBytes = upperInclusive.GetAddressBytes();
+            }
+
+            public bool IsInRange(IPAddress address)
+            {
+                if (address.AddressFamily != addressFamily)
+                {
+                    return false;
+                }
+
+                byte[] addressBytes = address.GetAddressBytes();
+
+                bool lowerBoundary = true, upperBoundary = true;
+
+                for (int i = 0; i < this.lowerBytes.Length &&
+                    (lowerBoundary || upperBoundary); i++)
+                {
+                    if ((lowerBoundary && addressBytes[i] < lowerBytes[i]) ||
+                        (upperBoundary && addressBytes[i] > upperBytes[i]))
+                    {
+                        return false;
+                    }
+                    lowerBoundary &= (addressBytes[i] == lowerBytes[i]);
+                    upperBoundary &= (addressBytes[i] == upperBytes[i]);
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ SimpleIPTools/IPv6.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace SimpleIPTools
+{
+    public static class IPv6
+    {
+        public static string ToUncompressedString(IPAddress ipAddress)
+        {
+            if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6)    // IPv6
+            {
+
+                var strings = Enumerable.Range(0, 8)    // create index
+                                        .Select(i => ipAddress.GetAddressBytes().ToList().GetRange(i * 2, 2))       // get 8 chunks of bytes
+                                        .Select(i => { i.Reverse(); return i; })    // reverse bytes for endianness
+                                        .Select(bytes => BitConverter.ToInt16(bytes.ToArray(), 0))  // convert bytes to 16 bit int
+                                        .Select(int16 => string.Format("{0:X4}", int16).ToUpper()); // format int as a 4 digit hex 
+
+                return string.Join(":", strings);   // join hex ints with ':'
+            }
+
+            return ipAddress.ToString();    // all else treat as to string
+        }
+
+        public class IPAddressRange
+        {
+            readonly AddressFamily addressFamily;
+            readonly byte[] lowerBytes;
+            readonly byte[] upperBytes;
+
+            public IPAddressRange(IPAddress lowerInclusive, IPAddress upperInclusive)
+            {
+                this.addressFamily = lowerInclusive.AddressFamily;
+                this.lowerBytes = lowerInclusive.GetAddressBytes();
+                this.upperBytes = upperInclusive.GetAddressBytes();
+            }
+
+            public bool IsInRange(IPAddress address)
+            {
+                if (address.AddressFamily != addressFamily)
+                {
+                    return false;
+                }
+
+                byte[] addressBytes = address.GetAddressBytes();
+
+                bool lowerBoundary = true, upperBoundary = true;
+
+                for (int i = 0; i < this.lowerBytes.Length &&
+                    (lowerBoundary || upperBoundary); i++)
+                {
+                    if ((lowerBoundary && addressBytes[i] < lowerBytes[i]) ||
+                        (upperBoundary && addressBytes[i] > upperBytes[i]))
+                    {
+                        return false;
+                    }
+                    lowerBoundary &= (addressBytes[i] == lowerBytes[i]);
+                    upperBoundary &= (addressBytes[i] == upperBytes[i]);
+                }
+                return true;
+            }
+        }
+    }
+}
